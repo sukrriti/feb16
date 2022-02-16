7714b6ca13998814ec9676be065f6e8dcec94e40
sukrriti <98798863+sukrriti@users.noreply.github.com>
2022-02-17 00:11:27 +0530

    commit 1st set2

---
+++ Set2.java
@@ -0,0 +1,10 @@
+public class Set2 {
+
+   public static void main(String[] args) {
+        
+      int num1 = 50, num2 = 50, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
